4b693cce653c83a4cd0716807b520f336bb541ac
andreaBidinost <andrea.bidinost.mail@gmail.com>
2024-05-27 08:36:47 +0000

    prove in classe

---
+++ INFORMATICA-3TELF/progetto27.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    cout << "BENVENUTO SU GIT POD!"<<endl;
+    string name;
+    cout << "Scrivi il tuo nome:"<<endl;
+    cin >> name;
+    cout << "Ciao " << name;
+    return 0;
+}
---
+++ MERN/server/main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    system("clear");
+    cout<<"prova"<<endl;
+    cout<<"ciao"<<endl;
+    int a;
+    cin >> a;
+    return 0;
+}
