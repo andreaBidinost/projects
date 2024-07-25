2898778f3c1acd75634bfe3d3d0ba5d02d860c1b
andreaBidinost <56701122+andreaBidinost@users.noreply.github.com>
2024-07-25 15:06:49 +0200

    deleted mw from here

--- MITS-WAREHOUSE/server/dbCredentials.php
+++
@@ -1,9 +0,0 @@
-<?php
-    class dbCredentials{
-        const SERVERNAME = 'localhost';
-        const USERNAME = 'root';
-        const PASSWORD = '';
-        const DBNAME = 'mitswarehouse';
-    }
-
-?>
--- MITS-WAREHOUSE/server/mock/code2description.php
+++
@@ -1,3 +0,0 @@
-<?php
-    echo json_encode(array('success' => true, 'id' =>432, 'pCode'=>'0A1FD','description' => 'Monitor Waveshare 7 pollici'));
-?>
